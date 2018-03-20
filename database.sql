341e5b13d97933bb4ef692dc98509f0dcc806543
Feora1343 <renee.vorbeck@gmail.com>
2018-03-20 17:18:04 -0500

    Database and login working

--- database.sql
+++ database.sql
@@ -1,93 +1,48 @@
 /*Schema as of 3/20/18 */
 
 CREATE TABLE "users" (
-	"user_id" serial NOT NULL,
-	"username" varchar(80) NOT NULL UNIQUE,
-	"password" varchar(240) NOT NULL,
-	"profile_image" varchar,
-	CONSTRAINT users_pk PRIMARY KEY ("user_id")
-) WITH (
-  OIDS=FALSE
+  "id" serial primary key,
+  "username" varchar(80) not null UNIQUE,
+  "password" varchar(240) not null
 );
-
-
 
 CREATE TABLE "campaign" (
-	"campaign_id" serial NOT NULL UNIQUE,
-	"campaign_name" varchar(50) NOT NULL UNIQUE,
-	"campaign_notes" varchar(3000),
-	"character_id" varchar(30),
-	"user_id" int NOT NULL UNIQUE,
-	CONSTRAINT campaign_pk PRIMARY KEY ("campaign_id")
-) WITH (
-  OIDS=FALSE
+  "campaign_id" serial NOT NULL UNIQUE,
+  "campaign_name" varchar(50) NOT NULL UNIQUE,
+  "campaign_notes" varchar(3000),
+  "character_id" varchar(30),
+  "user_id" int NOT NULL UNIQUE
 );
-
-
 
 CREATE TABLE "character" (
-	"character_id" serial NOT NULL,
-	"name" varchar(30) NOT NULL,
-	"character_icon" varchar(20) NOT NULL,
-	CONSTRAINT character_pk PRIMARY KEY ("character_id")
-) WITH (
-  OIDS=FALSE
+  "character_id" serial NOT NULL,
+  "name" varchar(30) NOT NULL,
+  "character_icon" varchar(20) NOT NULL,
+  CONSTRAINT character_pk PRIMARY KEY ("character_id")
 );
-
-
 
 CREATE TABLE "encounter" (
-	"encounter_id" serial NOT NULL,
-	"campaign_id" int NOT NULL,
-	CONSTRAINT encounter_pk PRIMARY KEY ("encounter_id")
-) WITH (
-  OIDS=FALSE
+  "encounter_id" serial NOT NULL,
+  "campaign_id" int NOT NULL,
+  CONSTRAINT encounter_pk PRIMARY KEY ("encounter_id")
 );
-
-
 
 CREATE TABLE "monster" (
-	"monster_id" serial NOT NULL,
-	"monster_name" varchar(30) NOT NULL UNIQUE,
-	"monster_icon" varchar(20) NOT NULL,
-	CONSTRAINT monster_pk PRIMARY KEY ("monster_id")
-) WITH (
-  OIDS=FALSE
+  "monster_id" serial NOT NULL,
+  "monster_name" varchar(30) NOT NULL UNIQUE,
+  "monster_icon" varchar(20) NOT NULL
 );
-
-
 
 CREATE TABLE "character_encounter" (
-	"character_id" serial NOT NULL,
-	"encounter_id" int NOT NULL,
-	CONSTRAINT character_encounter_pk PRIMARY KEY ("character_id")
-) WITH (
-  OIDS=FALSE
+  "character_id" serial NOT NULL,
+  "encounter_id" int NOT NULL
 );
-
-
 
 CREATE TABLE "monster_encounter" (
-	"monster_id" serial NOT NULL,
-	"encounter_id" int NOT NULL,
-	CONSTRAINT monster_encounter_pk PRIMARY KEY ("monster_id")
-) WITH (
-  OIDS=FALSE
+  "monster_id" serial NOT NULL,
+  "encounter_id" int NOT NULL
 );
 
 
-
-ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("user_id") REFERENCES "campaign"("user_id");
-
-
-ALTER TABLE "character" ADD CONSTRAINT "character_fk0" FOREIGN KEY ("character_id") REFERENCES "character_encounter"("character_id");
-
-ALTER TABLE "encounter" ADD CONSTRAINT "encounter_fk0" FOREIGN KEY ("campaign_id") REFERENCES "campaign"("campaign_id");
-
-ALTER TABLE "monster" ADD CONSTRAINT "monster_fk0" FOREIGN KEY ("monster_id") REFERENCES "monster_encounter"("monster_id");
-
-ALTER TABLE "character_encounter" ADD CONSTRAINT "character_encounter_fk0" FOREIGN KEY ("encounter_id") REFERENCES "encounter"("encounter_id");
-
-ALTER TABLE "monster_encounter" ADD CONSTRAINT "monster_encounter_fk0" FOREIGN KEY ("encounter_id") REFERENCES "encounter"("encounter_id");
 
 
