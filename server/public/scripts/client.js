6c0a2b1b6941b06b5fa6b5ba28a4f1b3f9313a2d
Feora1343 <renee.vorbeck@gmail.com>
2018-03-21 14:08:12 -0500

    multiple dialog popups working with fabtoolbar, not to fill in content for dialogs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,27 +6,74 @@ const myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngAria', 'ngMes
         count: 0,
         selectedDirection: 'left'
     }
+    $scope.changelogDialog = function(ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: '/views/dialogs/changelog.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
+
+    $scope.profileDialog = function(ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: '/views/dialogs/profile.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
+
+    $scope.aboutDialog = function(ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: '/views/dialogs/about.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
+
+    function DialogController($scope, $mdDialog) {
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+  
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+  
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+    }
   })
 
   setTimeout(function() {
     var elem = document.getElementsByClassName('pevents__initial')
     elem[0].classList.remove('pevents__initial')
     }, 2000);
-  // $scope.showAlert = function(ev) {
-  //   // Appending dialog to document.body to cover sidenav in docs app
-  //   // Modal dialogs should fully cover application
-  //   // to prevent interaction outside of dialog
-  //   $mdDialog.show(
-  //     $mdDialog.alert()
-  //       .parent(angular.element(document.querySelector('#popupContainer')))
-  //       .clickOutsideToClose(true)
-  //       .title('This is an alert title')
-  //       .textContent('You can specify some description text in here.')
-  //       .ariaLabel('Alert Dialog Demo')
-  //       .ok('Got it!')
-  //       .targetEvent(ev)
-  //   );
-  // };
+
 /// Routes ///
 myApp.config(['$routeProvider', '$locationProvider', '$mdIconProvider', function($routeProvider, $locationProvider, $mdIconProvider) {
   console.log('myApp -- config')
